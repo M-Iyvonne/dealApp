f3b004539513138dd6c6ee278d79029189fb8332
Jared Hensley <jahensle85@me.com>
2017-01-08 10:31:07 -0600

    jareds changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 //   "crossDomain": true,
 //   "url": "https://api.sqoot.com/v2/deals?api_key=DkDVEWgCHeXE-swLy6SD",
 //   "method": "GET",
-  
+
 // }
 
 // $.ajax(settings).done(function (response) {
@@ -12,36 +12,40 @@
 
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-$(".js-search-form").submit(function(){
-var keyword = $('.js-keyword').val();
-var location = $('.js-location').val();
-var radius = $('js.radius').val;
-var sqootUrl='"https://api.sqoot.com/v2/deals?api_key=DkDVEWgCHeXE-swLy6SD"&query='+keyword+'&location=' + location + '&radius=' + radius; 
+  $(".js-search-form").submit(function () {
+    var keyword = $('.js-keyword').val();
+    var location = $('.js-location').val();
+    var radius = $('.js-radius').val();
+    var sqootUrl = 'https://api.sqoot.com/v2/deals?api_key=DkDVEWgCHeXE-swLy6SD&query=' + keyword + '&location=' + location + '&radius=' + radius;
 
-$.ajax({
-  type: "GET",
-  url: sqootUrl,
-  dataType:"jsonp",
-  success: function(response){
+    $.ajax({
+      type: "GET",
+      url: sqootUrl,
+      dataType: "jsonp",
+      success: function (response) {
+        console.log(keyword, location, radius);
+        console.log("this is our response", response);
+        $('.js-results').html('');
+        if (response.deals.length) {
 
-  $('.js-results').html('');
-  if(response.deals.deal.length){
-  $.each(response.deals.deal, function(i,data){
-      var title=data.deals.deal.title;
-      var desc=data.deals.deal.description;
-      var url=data.deals.deal.url;
-      var final="<div class='deals'><div class='title'><a href='"+url+"'>"+ title +"</a></div><div class='description'>"+ desc +"</div><div class='url'>"+ url +"</div></div>";
-      console.log(response)
-  $('.js-results').append(final); 
 
-   });
-    }
-    else{
-    $('.js-results').html("<div id='no'>No Results</div>");
-    }
-    }
-});
-});
+          $.each(response.deals, function (i, data) {
+
+            // data.deals
+            var title = data.deal.title;
+            var desc = data.deal.description;
+            var url = data.deal.url;
+            var final = "<div class='deals'><div class='title'><a href='" + url + "'>" + title + "</a></div><div class='description'>" + desc + "</div><div class='url'>" + url + "</div></div>";
+
+            $('.js-results').append(final);
+
+          });
+        } else {
+          $('.js-results').html("<div id='no'>No Results</div>");
+        }
+      }
+    });
+  });
 });
